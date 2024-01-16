c2cc2d53c071e4af14c5e421021561af269dd572
jessica <2027felker.jessica@fcboe.com>
2024-01-16 14:32:54 -0500

    ascii art

---
+++ asciiart.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("\n");
+	printf("\n");
+	printf("    _____\n");
+	printf("   |     |\n");
+	printf("   |     |\n");
+	printf("   |     |\n");
+	printf(" ___________\n");
+	printf("|___________|\n");
+	printf("   O     O\n");
+	printf("      '\n");
+	printf("     ---\n");
+	printf("    |   |\n");
+	printf("    |   |\n");
+	printf("    |   |\n");
+	printf("     ---\n");
+	printf("\n");
+	printf("\n");
+	return 0;
+}
